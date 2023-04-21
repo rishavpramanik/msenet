0a618b0154923ae7f44e2b1144d9111631702bd4
Rishav Pramanik <rishavpramanik@gmail.com>
2023-04-22 02:22:03 +0530

    Update mse_ensemble.py

--- utils/mse_ensemble.py
+++ utils/mse_ensemble.py
@@ -22,26 +22,26 @@ def mse_ensemble(model1,model2,model3,x_train , y_train , x_val ,y_val):
     y_val = y_val.astype(int)
     finals = []
     for lists in List:
-    mean=np.zeros((len(np.unique(y_train))),dtype=float),
-    std=np.zeros((len(np.unique(y_train))),dtype=float)
-    probs=[[] for i in range(len(np.unique(y_train)))]
-    for i in range(len(lists[1])):
-      probs[y_train[i]].append(lists[1][i][y_train[i]])
-    mean=np.zeros((len(np.unique(y_train))),dtype=float)
-    std=np.zeros((len(np.unique(y_train))),dtype=float)
-    for i in range(len(probs)):
-      mean[i]=np.mean(probs[i])
-      std[i]= np.std(probs[i])
-    mean_T = temp_softmax([mean], T=2)
-    std_T = temp_softmax([std], T=2)
-    b = mean_T-std_T
-    a = [mean]/mean_T
-    X_val_final = a*lists[0] + b
-    finals.append(X_val_final)
+        mean=np.zeros((len(np.unique(y_train))),dtype=float),
+        std=np.zeros((len(np.unique(y_train))),dtype=float)
+        probs=[[] for i in range(len(np.unique(y_train)))]
+        for i in range(len(lists[1])):
+          probs[y_train[i]].append(lists[1][i][y_train[i]])
+        mean=np.zeros((len(np.unique(y_train))),dtype=float)
+        std=np.zeros((len(np.unique(y_train))),dtype=float)
+        for i in range(len(probs)):
+          mean[i]=np.mean(probs[i])
+          std[i]= np.std(probs[i])
+        mean_T = temp_softmax([mean], T=2)
+        std_T = temp_softmax([std], T=2)
+        b = mean_T-std_T
+        a = [mean]/mean_T
+        X_val_final = a*lists[0] + b
+        finals.append(X_val_final)
     finals = np.array(finals)
     summed = np.sum(finals, axis=0)
     ensembled_pred = np.argmax(summed, axis=1)
     accuracy_ens = accuracy_score(ensembled_pred, y_val)
     print(f"The accuracy after ensembling on the{fold_no+1}th fold: ", accuracy_ens)
-    return finals
+    return ensembled_pred
     
